d293e76657ec8cb3ad35cee5e4b03e28de32179d
Diqi Laptop <diqimusyaffa4@gmail.com>
2024-07-13 12:07:07 +0800

    Adding middleware, making roles and setup blade

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,7 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{-- {{ __('User Dashboard') }} --}}
             <div class="">{{ Auth::user()->name }} Dashboard</div>
         </h2>
     </x-slot>
